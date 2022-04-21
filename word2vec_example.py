e1d9d1c3e875296bb032e88c4331011eb80abf85
HB <hbheyhoo@163.com>
2022-04-21 20:35:34 +0800

    word embedding visualization.

--- word2vec_example.py
+++ word2vec_example.py
@@ -3,6 +3,10 @@ import codecs
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 import re
+import random
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import numpy as np
 
 import logging
 
@@ -68,7 +72,74 @@ def word2vec():
     print('训练完成')
 
 
+def tsne_plot(model):
+    "Creates and TSNE model and plots it"
+    labels = []
+    tokens = []
+    for word in model.wv.index_to_key:
+        tokens.append(model.wv[word])
+        labels.append(word)
+
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(16, 16))
+    for i in range(len(x)):
+        plt.scatter(x[i], y[i])
+        plt.annotate(labels[i],
+                     fontproperties=font,
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
+
+# 参考链接： https://blog.csdn.net/javastart/article/details/117714299
 if __name__ == '__main__':
     # wordSegmentation()
-    word2vec()
-    # jhekrqoplw;eqweqweqwe
+    # word2vec()
+    # 加载模型
+    model = Word2Vec.load('D:/StudyFile/ProjectWorkstation/EA/EntityAlignment/'
+               'dataset/Segment/in_the_name_of_people.word2vec')
+    # 计算相似性
+    print(model.wv.similarity('沙瑞金', '高育良'))
+    print(model.wv.similarity('李达康', '祁同伟'))
+
+    # 获取预料数量
+    print(model.corpus_count)
+
+    # 获取词语向量维度
+    print(model.wv.vector_size)
+
+    # 获取词向量
+    print(model.wv['李达康'])
+
+    # 词向量可视化
+    tsne_plot(model)
+
+    # words = list(model.wv.index_to_key)
+    # random.shuffle(words)
+    #
+    # vector = model.wv[words]
+    #
+    # tsne = TSNE(n_components=2, init='pca', verbose=1)
+    # embedd = tsne.fit_transform(vector)
+    #
+    # plt.figure(figsize=(14, 10))
+    # plt.scatter(embedd[:300, 0], embedd[:300, 1])
+    #
+    # for i in range(300):
+    #     x = embedd[i][0]
+    #     y = embedd[i][1]
+    #     plt.text(x, y, words[i])
+    # plt.show()
+
+
+
+
